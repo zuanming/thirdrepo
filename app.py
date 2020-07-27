948b57dc9bbfa5947ce1b65e04f45feb5dfa18b1
zuanming <zuanming@gmail.com>
2020-07-27 17:17:35 +0000

    updated css colors

--- app.py
+++ app.py
@@ -272,6 +272,3 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-    # app.run(host='0.0.0.0',
-    #         port=8080,
-    #         debug=True)
